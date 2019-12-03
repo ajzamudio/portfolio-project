cdc1bb2977389428b10fae1ea9dc47345ccf5e2c
Zamudio <Aaron.Zamudio@ehi.com>
2019-12-03 14:22:17 -0600

    finished website

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\e589pv\ideaprojects\portfolio-project\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
